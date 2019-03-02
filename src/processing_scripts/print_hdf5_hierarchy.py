4e8d2c9656e8b42e4ac4030a9597d3b4d7e5fc25
craicrai <pfg@posteo.org>
2019-03-02 17:46:37 +0000

    Clearer printing of hdf5 file hierarchy

--- src/processing_scripts/print_hdf5_hierarchy.py
+++ src/processing_scripts/print_hdf5_hierarchy.py
@@ -1,12 +1,13 @@
 """
 Print the entire hierarchy of a hdf5 file
 
-Run with: python print_hdf5_hierarchy.py
+Run with: python print_hdf5_hierarchy.py FILENAME
 
 """
 
 
 import h5py
+import sys
 
 
 def print_hdf5_hierarchy(file_name):
@@ -14,20 +15,21 @@ def print_hdf5_hierarchy(file_name):
     file = h5py.File(file_name, 'r')
     print(file.name)
 
-#    offset = '    '
-#    i = 1
-    def print_members(group):
+    def print_members(group, level):
         for m in group.__iter__():
-            print(m)
             if isinstance(group[m], h5py.Group):
-                print('    ', print_members(group[m]))
+                print('{0}{1}{2}{1}'.format('    ' * level, '/', m))
+                level += 1
+                print_members(group[m], level)
+            else:
+                print('{0}{1}{2}'.format('    ' * level, '/', m))
                 
-    print_members(file)
+    print_members(file, 0)
     
     file.close()
 
     
-print_hdf5_hierarchy('../../data/cmos-72532_FOR_TESTS.hdf')
+print_hdf5_hierarchy(sys.argv[1])
 
 
 #def test_print_hdf5_structure:
