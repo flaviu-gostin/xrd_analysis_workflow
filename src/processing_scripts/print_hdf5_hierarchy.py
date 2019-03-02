657af6d9c52e8d7b3a000a459b743589fd1a5c28
craicrai <pfg@posteo.org>
2019-03-02 19:59:47 +0000

    Print tree inside hdf5 file using visititems() method

--- src/processing_scripts/print_hdf5_hierarchy.py
+++ src/processing_scripts/print_hdf5_hierarchy.py
@@ -10,27 +10,22 @@ import h5py
 import sys
 
 
-def print_hdf5_hierarchy(file_name):
-    """Print the entire hierarchy of a hdf5 file"""
-    file = h5py.File(file_name, 'r')
-    print(file.name)
-
-    def print_members(group, level):
-        for m in group.__iter__():
-            if isinstance(group[m], h5py.Group):
-                print('{0}{1}{2}{1}'.format('    ' * level, '/', m))
-                level += 1
-                print_members(group[m], level)
-            else:
-                print('{0}{1}{2}'.format('    ' * level, '/', m))
-                
-    print_members(file, 0)
-    
-    file.close()
-
-    
-print_hdf5_hierarchy(sys.argv[1])
+# Open the hdf5 file
+file = h5py.File(sys.argv[1], 'r')
 
 
-#def test_print_hdf5_structure:
-#    struct = print_hdf5_structure()
+def find_all(name, object):
+    """ What to print for each member of the tree """
+
+    if isinstance(object, h5py.Dataset):    # if the member is a Dataset ...
+        if object.shape == (1,):    # has one dimension with one single value
+            print(name, '(Dataset, value =', object.value, ')')    # print value
+        else:
+            print(name, '(Dataset, shape:', object.shape, ', type:', object.dtype, ')')    # otherwise print the shape and type 
+
+    elif isinstance(object, h5py.Group):
+        print(name, '(Group)')
+
+file.visititems(find_all)
+
+file.close()
