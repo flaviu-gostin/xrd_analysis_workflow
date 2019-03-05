f950f11381d481f46b64d116d318c307d60ae607
craicrai <pfg@posteo.org>
2019-03-05 16:23:27 +0000

    Open hdf5 file inside the function

--- src/functions/get_slice.py
+++ src/functions/get_slice.py
@@ -8,17 +8,19 @@ import h5py
 import numpy as np
 
 
-def get_slice(file, n):
+def get_slice(filename, n):
     """First slice is 0, second is 1, ..."""
+    file = h5py.File(filename, 'r')
     all_slices = file['entry/instrument/detector/data']
     slice = all_slices[n]
+    file.close()
     return slice
 
 
 def test_slice():
     """Test function get_slice(file, n)"""
-    file = h5py.File('../../../data/cmos-72532_FOR_TESTS.hdf', 'r')
-    a_slice = get_slice(file, 71)
+    filename = "../../data/cmos-72532_FOR_TESTS.hdf"
+    a_slice = get_slice(filename, 71)
     assert a_slice.ndim == 2
     assert a_slice.shape == (2045, 4098)
     assert a_slice.dtype == 'uint16'
