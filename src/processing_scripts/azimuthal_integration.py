68fd268db6402e45773e9004317b8f9077c794a9
craicrai <pfg@posteo.org>
2019-03-10 22:57:39 +0000

    First working azimuthal integration. Tested on PS1p3V

---
+++ src/processing_scripts/azimuthal_integration.py
@@ -0,0 +1,37 @@
+"""This script performs azimuthal integration.
+
+It takes a list of hdf files and creates a 2theta-intensity dat file for each 
+slice in each hdf file.
+"""
+
+
+import numpy as np
+import pyFAI
+import h5py
+import sys
+import os
+
+
+location_hdfs = "../../data/"
+# Names of hdf files to integrate
+names = ["PS_1p3V"]
+location_results = "../../results/intermediate/integrated_1D/"
+poni_file = "../../results/intermediate/calibration/Si_17.95keV.poni"
+
+
+ai = pyFAI.load(poni_file)
+
+for name in names:
+    hdf_file = location_hdfs + name + ".hdf"
+    file = h5py.File(hdf_file, 'r')
+    dataset = file['entry/instrument/detector/data']
+
+    result_file_dir = location_results + name
+    if not os.path.exists(result_file_dir):
+        os.mkdir(result_file_dir)
+        
+    for idx, slice in enumerate(dataset):
+        result_file = result_file_dir + "/" + str(idx) + ".dat"
+        ai.integrate1d(slice, 4153, unit="2th_deg", filename=result_file)
+
+    file.close()
