cf1fd6cd74f166a2a2a4da2cbda5bae94b82c1e2
craicrai <pfg@posteo.org>
2019-03-10 20:59:19 +0000

    Tidy up

--- src/image_scripts/raw_diffr_images.py
+++ src/image_scripts/raw_diffr_images.py
@@ -1,38 +1,25 @@
-"""Creates raw diffraction image"""
+"""Creates raw diffraction images for the manuscript"""
 
-print("Generating raw diffraction images ...")
 
 import matplotlib.pyplot as plt
 import sys
-
-
-location_of_data = "../../data/"
-location_of_images = "../../images/"
-filename1 = "cmos-72532_FOR_TESTS.hdf"
-file1 = location_of_data + filename1
-# filename2 = ...
-# file2 = ...
-slices_to_plot = [(file1, 71),
-                  (file1, 72)]    # [(file, slice no), ...]
 location_of_functions = "../functions/"
-
-
 sys.path.append(location_of_functions)
-
 from get_slice import get_slice
 
 
-for (file, slice) in slices_to_plot:
+location_of_data = "../../data/"
+location_of_images = "../../results/final/"
+slices_to_plot = [("PS_1p3V", 71)]    # [(file, slice no), ...]
+
+
+for (condition, slice) in slices_to_plot:
+    file = location_of_data + condition + ".hdf"
     slc = get_slice(file, slice)
     plt.imshow(slc, cmap="gray")
     plt.xticks(())
     plt.yticks(())
-    plt.title(file + ", slice " +  str(slice))
-    filename = file.split("/")[-1]
-    file_no = filename[5:10]
-    save_file = file_no + "_" + str(slice) + "_raw.png"
-    plt.savefig(location_of_images + save_file)
+    plt.title(condition + ", slice " +  str(slice))
+    save_filename = condition + "_slice-" + str(slice) + "_raw.png"
+    plt.savefig(location_of_images + save_filename, dpi=500)
     plt.close()
-    print("Saved", save_file)
-
-print("Done")
