02e87ef602711387b0910a4853e82008bc23b837
craicrai <pfg@posteo.org>
2019-06-04 18:40:22 +0100

    Major code reorganisation, e.g. moved the averaging of lattice constants from peak_calc.py to Pd_summary.py

--- src/functions/peak_calc.py
+++ src/functions/peak_calc.py
@@ -5,13 +5,10 @@ import pytest
 import os
 import natsort
 
-wl = 0.6907e-10    # Wavelength in meter
-roi_Pd113 = (33.5, 34.6)    # (start, end) values of 2theta interval of Pd113
 
-
-def latt_ct(wavelength, peak_pos):
+def latt_ct(wavelength, peak_pos, cryst_system, planes):
     """
-    Calculate constant of cubic lattice from 113 peaks.
+    Calculate lattice constant of cubic systems from 113 peaks.
     
     Calculate the constant of a cubic lattice from the position of the
     113 peak using Bragg's law and simple geometry.
@@ -23,6 +20,13 @@ def latt_ct(wavelength, peak_pos):
         diffraction pattern.
     peak_pos : float
         The position of the 113 diffraction peak in degree.
+   cryst_system : string
+       String describing the crystal system of the phase for which the
+       lattice constant is calculated.
+   planes : tuple
+       Tuple with 3 integers representing the Miller indices (h, k, l)
+       of the planes used for calculating the lattice constant.
+
 
     Returns
     -------
@@ -33,6 +37,9 @@ def latt_ct(wavelength, peak_pos):
     if wavelength < 1e-12 or wavelength > 1e-9:
         raise ValueError(str(wavelength) + " (meter) is an unlikely\
         value for the wavelength")
+    if cryst_system != "cubic":
+        raise ValueError("At the moment this works only for cubic\
+                         systems")
     # Change unit to Angstrom
     wl = wavelength * 1e+10    
     # Calculate theta in radian
@@ -40,16 +47,16 @@ def latt_ct(wavelength, peak_pos):
     # d is interplanar spacing from Bragg's law
     d = wl / (2 * np.sin(theta))
     # a is lattice constant
-    a = d * np.sqrt(1**2 + 1**2 + 3**2)
+    a = d * np.sqrt(planes[0]**2 + planes[1]**2 + planes[2]**2)
     return a
 
 
-def peak_positions(dir_1D, patterns):
+def peak_maxima(dir_1D, patterns, tth_interval):
     """
-    Determine the Pd113 peak positions for given patterns.
+    Determine two-theta of peak maximum for given patterns.
 
-    For each pattern in given patterns determine the two-theta value of
-    the highest intensity in the Pd113 region.
+    For each given pattern determine the two-theta value of the highest
+    intensity in the given interval.
 
     Parameters
     ----------
@@ -57,20 +64,26 @@ def peak_positions(dir_1D, patterns):
         Directory where the 1D diffraction pattern are
     patterns : tuple
         Tuple with 2 integers (first pattern, last pattern + 1)
+    tth_interval : tuple
+        Tuple with 2 float objects representing the left and right
+        margins of the two-theta interval in which the peak is located.
 
     Returns
     -------
-    out : list
-        List containing two-theta values in degrees representing the
-    maxima of Pd113 peaks.
+    twotheta_peaks : list
+        List containing two-theta values in degrees, one value for each
+        pattern.  Each value corresponds to the maximum intensity in the
+        given interval.
 
     """
     # twotheta_peaks is a list collecting peak maxima
     twotheta_peaks = []
-    # Determine array indices for roi_Pd113 from the first file in directory
+    # Use the 1st pattern to determine indices of tth_interval
     patt0 = np.loadtxt(dir_1D + str(patterns[0]) + ".dat",
                        usecols=(0,1))
-    st, end = get_indices(patt0, roi_Pd113[0], roi_Pd113[1])
+    st = get_idx(patt0, tth_interval[0])
+    end = get_idx(patt0, tth_interval[1])
+    # List of files (each file contains a pattern) sorted numerically
     files = \
     natsort.natsorted(os.listdir(dir_1D))[patterns[0]:patterns[1]]
     for file in files:
@@ -83,52 +96,32 @@ def peak_positions(dir_1D, patterns):
     return twotheta_peaks
 
     
-def get_indices(array_a, twoth_st, twoth_end):
-    """ Determine the start and end indices of given 2theta interval 
-
-    array_a: 2D array ([[2theta values list], [intensity values list]])
-    (start, end): start and end 2theta values of given interval
-
+def get_idx(array_a, twoth):
     """
-
-    twoth_axis = array_a[..., 0]    # Get the X axis (two theta) values
-    diff_st = abs(twoth_axis - twoth_st)   # Subtract Pd113 start two theta value
-    st_idx = np.argmin(diff_st)    # Index of roi_Pd113 start two theta 
-
-    diff_end = abs(twoth_axis - twoth_end)
-    end_idx = np.argmin(diff_end)
-
-    return (st_idx, end_idx)
-
-
-def latt_ct_avg(dir_1D, patterns, wl):
-    """
-    Calculate average lattice constant of Pd.
-
-    Calculates the average lattice constant of Pd from the Pd113 peak for each 
-    pattern and then calculates the average over all given patterns.
+    Determine the index of 2theta which is nearest to given 2theta.
 
     Parameters
     ----------
-    dir_1D : string
-        Directory where the 1D diffraction pattern are
-    patterns : tuple
-        Tuple with 2 integers (first pattern, last pattern + 1)
-    wl : float
-        Wavelength in meter.
+    array_a : ndarray
+        Numpy 2-dimensional array ([[2theta values list],
+                                    [intensity values list]]).
+    twoth : float
+        Value of 2theta for which we want a (near) index.
 
     Returns
     -------
-    out : float
-        Float number representing the average of the Pd lattice
-    constant.
+    idx : integer
+        Index in array_a of the 2theta value which is nearest to the
+        given twoth
 
-    """ 
-    latt_ct_sum = 0    # lattice constant summed over all patterns
-    for tth in peak_positions(dir_1D, patterns):
-        latt_ct_sum += latt_ct(wl, tth)
-    latt_ct_avg = latt_ct_sum / len(peak_positions(dir_1D, patterns))
-    return latt_ct_avg
+    """
+    twoth_axis = array_a[..., 0]    # Get the X axis (two theta) values
+    # Subtract given twoth from each 2theta value in twoth_axis
+    diff_2th = abs(twoth_axis - twoth)   
+    # idx is the index of the smallest item in diff_2th (= index of
+    # 2theta closest to the given 2theta)     
+    idx = np.argmin(diff_2th)
+    return idx
 
 
 def test_latt_ct():
--- src/table_scripts/Pd_summary.py
+++ src/table_scripts/Pd_summary.py
@@ -7,36 +7,58 @@ import numpy as np
 import os
 import sys
 sys.path.append("../functions/")
-from peak_calc import latt_ct_avg 
+from peak_calc import peak_maxima, latt_ct
 
 wl = 0.6907e-10    # Wavelength in meter
-roi_Pd113 = (33.5, 34.6)    # (start, end) values of 2theta interval of Pd113
+cryst_syst_Pd = "cubic"    # Crystal system of Pd
+# (start, end) values of 2theta interval of Pd113
+tth_int_Pd113 = (33.5, 34.6)
+planes_Pd113 = (1, 1, 3)
 location_stacks = "../../results/intermediate/integrated_1D/"
 
 """scans_patterns is a list giving the directories in location_stacks
 where 1D patterns are, and patterns used for calculating the average
 lattice spacing of Pd as a tuple (first pattern, last pattern + 1, step)"""
 scans_patterns = [["PS_1p3V_b", (0, 82)],
-                  # ["PSA_1p3V_c", (0, 44)],
-                  # ["PSP_1p3V_b", (0, 63)],
-                  # ["PSAP_1p3V_a", (10, 31)],
-                  # ["PS_0p7V_b", (0, 69)],
-                  # ["PS_0p5V_b", (0, 119)],
-                  # ["PS_0p0V_a", (143, 148)],
-                  # ["PS_0p0V_a", (69, 74)],
+                  ["PSA_1p3V_c", (0, 44)],
+                  ["PSP_1p3V_b", (0, 63)],
+                  ["PSAP_1p3V_a", (10, 31)],
+                  ["PS_0p7V_b", (0, 69)],
+                  ["PS_0p5V_b", (0, 119)],
+                  ["PS_0p0V_a", (143, 148)],
+                  ["PS_0p0V_a", (69, 74)],
                   ["PS_0p0V_a", (0, 5)]]
-
 results_file = "../../results/final/table_Pd_summary.txt"
 
+table_header = \
+"""
+Table.  Lattice constant and crystallite size of Pd particles
+generated inside artificial pits on Ti40Zr10Cu34Pd14Sn2 metallic glass
+ribbons.
+---------------------------------------------
+{:<15}{:<12}{}
+---------------------------------------------
+""".format("Electrolyte", "Potential", "Lattice constant")
 
 with open(results_file, "w") as rf:
-    rf.write("# This is a summary table of lattice spacing values determined from the Pd113 peak \n")
+    rf.write(table_header)
     for [scan, patterns] in scans_patterns:
         dir_1D = location_stacks + scan + "/"
-        lca = latt_ct_avg(dir_1D, patterns, wl)
-        #e is the electrolyte
-        e = scan.split('_')[0]
-        #p is the potential
-        p = scan.split('_')[1]
-        rf.write(e + "     " + p + "    " +
-                 str(np.around(lca, decimals=4)) + "\n")
+        # pm contains 2theta values of Pd113 peak max for each pattern
+        pm = peak_maxima(dir_1D, patterns, tth_int_Pd113)
+        # lc calculates lattice constant values from 2theta values in pm
+        lc = [latt_ct(wl, i, cryst_syst_Pd, planes_Pd113) for i in pm]
+        lca = np.mean(lc, dtype=np.float64)    # Average over lc
+        lcsd = np.std(lc, dtype=np.float64)    # St dev over lc
+        lcsd_1signif = '{:.1g}'.format(lcsd)    # Round to 1 signif digit
+        # Get the no of decimals of lcsd_signif
+        decimals_no = len(str(lcsd_1signif).split('.')[1])
+        # ... and apply it to lca
+        lca_signif = '{:.{dec_no}f}'.format(lca, dec_no=decimals_no)
+        # lcsd_1 is a single digit: the first significant figure
+        lcsd_1 = str(lcsd_1signif)[-1]
+        e = scan.split('_')[0]    # the electrolyte
+        p = scan.split('_')[1]    # the potential
+        p_better = p.replace("p", ".")
+        rf.write("{:<15}{:<12}{}({})\n".format(e, p_better, lca_signif,
+                                               lcsd_1))
