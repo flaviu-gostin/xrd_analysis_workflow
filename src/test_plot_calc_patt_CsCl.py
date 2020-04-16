853811a63f3f92b9ca677b7d5bf5af6a2f8cda2b
Flaviu Gostin <pfg@posteo.org>
2020-04-16 13:00:12 +0100

    Add script to manually test creation and plotting of calculated pattern

---
+++ src/test_plot_calc_patt_CsCl.py
@@ -0,0 +1,28 @@
+""" This script plots a calculated diffraction pattern of CsCl.
+
+This is for testing purposes.
+
+"""
+
+from pymatgen import Lattice, Structure
+import sys
+sys.path.append("functions/")
+from calculate_pattern import calculate_pattern
+from plot_calculated_pattern import plot_calculated_pattern
+
+
+wavelength = 1.0
+two_theta_range = (0, 90)
+# Create CsCl structure
+a = 4.209 # in Angstrom
+latt = Lattice.cubic(a)
+structure = Structure(latt, ["Cs", "Cl"], [[0, 0, 0], [0.5, 0.5, 0.5]])
+(two_th, intensity) = calculate_pattern(structure, wavelength, two_theta_range)
+
+savefile = "plot_calc_CsCl-delete_me.png"
+plot_calculated_pattern(two_th, intensity, 'blue', savefile)
+print("File {} should have been created".format(savefile))
+print("You may delete it")
+print("The plot should also be shown in a window")
+
+plot_calculated_pattern(two_th, intensity, 'blue')
