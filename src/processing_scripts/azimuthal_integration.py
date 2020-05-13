7cbaf9e231f52924fde784d7d2fd24875c9a9b35
Flaviu Gostin <pfg@posteo.org>
2020-05-13 21:58:48 +0100

    Refactor azimuthal_integration.py to work with command line arguments

--- src/processing_scripts/azimuthal_integration.py
+++ src/processing_scripts/azimuthal_integration.py
@@ -1,7 +1,9 @@
 """This script performs azimuthal integration.
 
-It takes a list of hdf files and creates a 2theta-intensity dat file
-for each slice in each hdf file.
+It takes a hdf file and creates a 2theta-intensity dat file for each slice in
+that hdf file using the refined geometry in a poni file.
+
+Usage: python azimuthal_integration.py poni-file hdf-file results-dir
 
 """
 
@@ -9,50 +11,24 @@ import numpy as np
 import pyFAI
 import h5py
 import sys
-import os
-
-location_hdfs = "../../data/"
-# Names of hdf files to integrate
-names = ["PS_1p3V_a",
-         "PS_1p3V_b",
-         "PS_1p3V_c",
-         "PS_0p7V_a",
-         "PS_0p7V_b",
-         "PS_0p5V_a",
-         "PS_0p5V_b",
-         "PS_0p0V_a",
-         "PSA_1p3V_a",
-         "PSA_1p3V_b",
-         "PSA_1p3V_c",
-         "PSA_1p3V_d",
-         "PSAP_1p3V_a",
-         "PSAP_1p3V_b",
-         "PSAP_1p3V_c",
-         "PSAP_1p3V_d",
-         "PSAP_1p3V_e",
-         "PSP_1p3V_a",
-         "PSP_1p3V_b",
-         "PSP_1p3V_c"]
-location_results = "../../results/intermediate/integrated_1D/"
-poni_file = "../../results/intermediate/calibration/Si_17.95keV.poni"
 
 
+poni_file = sys.argv[1]
+# create azimuthal integrator object
 ai = pyFAI.load(poni_file)
 
-print("Integrating diffraction images in:")
-for name in names:
-    print(name + "...")
-    hdf_file = location_hdfs + name + ".hdf"
-    file = h5py.File(hdf_file, 'r')
-    dataset = file['entry/instrument/detector/data']
 
-    result_file_dir = location_results + name
-    if not os.path.exists(result_file_dir):
-        os.mkdir(result_file_dir)
-        
-    for idx, slice in enumerate(dataset):
-        result_file = result_file_dir + "/" + str(idx) + ".dat"
-        ai.integrate1d(slice, 4153, unit="2th_deg", filename=result_file)
+hdf_filename = sys.argv[2]
+# print("Integrating diffraction images in:", hdf_filename)
+hdf_file = h5py.File(hdf_filename, 'r')
+diffraction_images = hdf_file['entry/instrument/detector/data']
 
-    file.close()
-    print("Done")
+results_dir = sys.argv[3]
+for idx, image in enumerate(diffraction_images):
+    result_filename = results_dir + "/" + str(idx) + ".dat"
+    ai.integrate1d(image, 4153, unit="2th_deg", filename=result_filename)
+
+hdf_file.close()
+
+
+# "image" would be "slice" in DAWN terminology (perhaps in hdf terminology too)
