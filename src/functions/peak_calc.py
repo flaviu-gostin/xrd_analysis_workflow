71477cada03174b4a67cc2f602aaaf09d4943ba2
Flaviu Gostin <pfg@posteo.org>
2020-06-28 15:07:45 +0100

    Correct and improve docstrings for several functions

--- src/functions/peak_calc.py
+++ src/functions/peak_calc.py
@@ -7,11 +7,11 @@ import natsort
 
 
 def latt_ct(wavelength, peak_pos, cryst_system, planes):
-    """
-    Calculate lattice constant of cubic systems from 113 peaks.
-    
-    Calculate the constant of a cubic lattice from the position of the
-    113 peak using Bragg's law and simple geometry.
+    """Calculate lattice constant of cubic systems.
+
+    Calculate the lattice constant for a cubic lattice from the position of
+    given peak position as 2theta and given hkl plane using Bragg's law and
+    simple geometry.
 
     Parameters
     ----------
@@ -19,13 +19,13 @@ def latt_ct(wavelength, peak_pos, cryst_system, planes):
         The value of the wavelength in meter used to measure the
         diffraction pattern.
     peak_pos : float
-        The position of the 113 diffraction peak in degree.
-   cryst_system : string
-       String describing the crystal system of the phase for which the
-       lattice constant is calculated.
-   planes : tuple
-       Tuple with 3 integers representing the Miller indices (h, k, l)
-       of the planes used for calculating the lattice constant.
+        The 2theta position of the diffraction peak in degree.
+    cryst_system : string
+        String describing the crystal system of the phase for which the
+        lattice constant is calculated.
+    planes : tuple
+        Tuple with 3 integers representing the Miller indices (h, k, l)
+        of the planes used for calculating the lattice constant.
 
 
     Returns
@@ -41,7 +41,7 @@ def latt_ct(wavelength, peak_pos, cryst_system, planes):
         raise ValueError("At the moment this works only for cubic\
                          systems")
     # Change unit to Angstrom
-    wl = wavelength * 1e+10    
+    wl = wavelength * 1e+10
     # Calculate theta in radian
     theta = ((peak_pos / 2) * np.pi ) / 180
     # d is interplanar spacing from Bragg's law
@@ -52,11 +52,12 @@ def latt_ct(wavelength, peak_pos, cryst_system, planes):
 
 
 def peak_maxima(dir_1D, patterns, tth_interval):
-    """
-    Determine two-theta of peak maximum for given patterns.
+    """Determine two-theta of peak maximum for given patterns.
 
     For each given pattern determine the two-theta value of the highest
-    intensity in the given interval.
+    intensity in the given interval.  Note: it is assumed that the two-theta
+    axis is the same for all patterns, i.e. patterns[0][..., 0] ==
+    patterns[1][..., 0] == ...
 
     Parameters
     ----------
@@ -95,7 +96,7 @@ def peak_maxima(dir_1D, patterns, tth_interval):
         twotheta_peaks.append(twotheta_peak)
     return twotheta_peaks
 
-    
+
 def get_idx(array_a, twoth):
     """
     Determine the index of 2theta which is nearest to given 2theta.
@@ -103,8 +104,9 @@ def get_idx(array_a, twoth):
     Parameters
     ----------
     array_a : ndarray
-        Numpy 2-dimensional array ([[2theta values list],
-                                    [intensity values list]]).
+        Numpy 2-dimensional array ([[2theta, intensity],
+                                    [2theta, intensity],
+                                    ...                ]).
     twoth : float
         Value of 2theta for which we want a (near) index.
 
@@ -117,9 +119,9 @@ def get_idx(array_a, twoth):
     """
     twoth_axis = array_a[..., 0]    # Get the X axis (two theta) values
     # Subtract given twoth from each 2theta value in twoth_axis
-    diff_2th = abs(twoth_axis - twoth)   
+    diff_2th = abs(twoth_axis - twoth)
     # idx is the index of the smallest item in diff_2th (= index of
-    # 2theta closest to the given 2theta)     
+    # 2theta closest to the given 2theta)
     idx = np.argmin(diff_2th)
     return idx
 
