627a7ff5075ccc8f4f22b2fe57fdce94f1475fc5
Flaviu Gostin <pfg@posteo.org>
2020-07-29 11:31:06 +0300

    Offset lines from each other to improve visibility

--- src/image_scripts/plot_diffraction_patterns.py
+++ src/image_scripts/plot_diffraction_patterns.py
@@ -69,9 +69,9 @@ for idx, fn in enumerate(sorted_fns):
         label.set_fontsize('xx-small')
 
 #label layers, e.g. "Pd", "PdCl2"
-for k, v in layers.items():
+for idx, (k, v) in enumerate(layers.items()):
     #add upper and (empty) lower annotation boxes
-    label_text_top, label_text_bottom = k, 'x' + k
+    label_text_top, label_text_bottom = k, '' #'x' + k
     data_top = np.loadtxt(os.path.join(measured_patterns_dir, str(v[0]) +
                                        '.dat'))
     data_bottom = np.loadtxt(os.path.join(measured_patterns_dir, str(v[1]) +
@@ -92,11 +92,12 @@ for k, v in layers.items():
     y_vals_bottom[idx_rightmost_point_bottom]
     ann_top = ax_measured.annotate(label_text_top, (x_ref_top, y_ref_top),
                                    textcoords="offset points",
-                                   xytext = (2 + 12 , 0), va='center')
+                                   xytext = (2 + 12 + idx * 4, 0), va='center')
     ann_bottom = ax_measured.annotate(label_text_bottom,
                                       (x_ref_bottom, y_ref_bottom),
                                       textcoords="offset points",
-                                      xytext = (2 + 12 , 0), va='center')
+                                      xytext = (2 + 12 + idx * 4, 0),
+                                      va='center')
 
     #add line between upper and lower annotation boxes
     ax_measured.annotate('', xy=(0, 0.5), xycoords=ann_top, xytext=(0, 0.5),
