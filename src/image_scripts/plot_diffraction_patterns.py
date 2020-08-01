flaviu-gostin/xrd_analysis_workflow

commit c92fc7a3b44eb3525764a68b6a98219174435dc2

Add stick plot for CuCl reference

src/image_scripts/plot_diffraction_patterns.py
@@ -20,7 +20,8 @@ import sys
 measured_patterns_dir = "../../results/intermediate/integrated_1D/PS_1p3V_b"
 reference_peaks_dir = "../../results/intermediate/peaks_references"
 references_fnames = {'Pd': 'Pd.dat',
-                     'PdCl2': 'PdCl2.dat'}
+                     'PdCl2': 'PdCl2.dat',
+                     'CuCl': 'CuCl.dat'}
 figure_fn = "diffraction_patterns.svg"
 
 measured_patterns_fns_unsorted = os.listdir(measured_patterns_dir)
@@ -56,10 +57,10 @@ mpl.rcParams['axes.linewidth'] = global_linewidth
 #plt.rcParams.update({'figure.autolayout': True})
 
 
-fig, ax = plt.subplots(nrows=3, sharex=True,
-                       gridspec_kw=dict(height_ratios=[1, 1, 5]))
-#3rd cell is 5x higher than 1st and 2nd
-ax_ref1, ax_ref2, ax_measured = ax
+fig, ax = plt.subplots(nrows=4, sharex=True,
+                       gridspec_kw=dict(height_ratios=[1, 1, 5,1]))
+#3rd cell is 5x higher than 1st, 2nd and 4th
+ax_ref1, ax_ref2, ax_measured, ax_ref3 = ax
 #fig.set_dpi(500)    useless for vector graphics?
 fig.set_figwidth(figwidth)
 fig.set_figheight(figheight)
@@ -70,7 +71,7 @@ fig.subplots_adjust(left=0.06, right=0.755, bottom=0.085, top=0.995,
 #ax_measured.set_box_aspect(3/1) #Don't use this.  Use gridspec_kw in fig
 ax_measured.tick_params(axis='y', which='both', left=False, labelleft=False)
 ax_measured.set_ylabel("Relative intensity")
-ax_measured.set(xlim=[2, 41], xlabel="2theta, degree")
+plt.setp(ax_measured.get_xticklabels(), visible=False)
 
 
 #plot data and label patterns with numbers
@@ -127,7 +128,7 @@ for idx, (k, v) in enumerate(layers.items()):
                                      'linewidth': global_linewidth})
 
 
-# add stick plot(s) for reference phases
+# Add stick plot(s) for reference phases
 #ax_ref1.set_box_aspect(1/4) #Don't use this.  Use gridspec_kw in fig
 ax_ref1.tick_params(axis='y', which='both', left=False, labelleft=False)
 plt.setp(ax_ref1.get_xticklabels(), visible=False)
@@ -149,6 +150,16 @@ stem_container.baseline.set_visible(False)
 stem_container.markerline.set_visible(False)
 stem_container.stemlines.set_color('red')
 
+ax_ref3.tick_params(axis='y', which='both', left=False, labelleft=False)
+#plt.setp(ax_ref3.get_xticklabels(), visible=False)
+ax_ref3.set(xlim=[2, 41], xlabel="2theta, degree")
+ax_ref3.set(ylim=[0, 110])
+data = np.loadtxt(os.path.join(reference_peaks_dir, references_fnames['CuCl']))
+twotheta, intensity = data[:,0], data[:,1]
+stem_container = ax_ref3.stem(twotheta, intensity)
+stem_container.baseline.set_visible(False)
+stem_container.markerline.set_visible(False)
+stem_container.stemlines.set_color('green')
 
 fig.savefig(figure_fn)
 #plt.grid()

commit 1f2f489c371777f0c472840cabdc7f5ccf59c0f4

Fix missing labels bug (introduced by c92fc7a)

src/image_scripts/plot_diffraction_patterns.py
@@ -37,6 +37,7 @@ except IndexError:
 
 patterns_to_plot = list(range(start, stop))
 
+twotheta_range = [2, 41]
 offset_patterns = 2000
 label_every_nth_pattern = 5
 
@@ -72,7 +73,7 @@ fig.subplots_adjust(left=0.06, right=0.755, bottom=0.085, top=0.995,
 ax_measured.tick_params(axis='y', which='both', left=False, labelleft=False)
 ax_measured.set_ylabel("Relative intensity")
 plt.setp(ax_measured.get_xticklabels(), visible=False)
-
+ax_measured.set(xlim=twotheta_range)
 
 #plot data and label patterns with numbers
 for pattern_no in patterns_to_plot:
@@ -85,6 +86,7 @@ for pattern_no in patterns_to_plot:
         label_text = str(pattern_no)
         idx_rightmost_point = np.searchsorted(x_vals, ax_measured.get_xlim()[1],
                                               side='right') - 1
+        #xlim shouldn't be changed after this point (might affect labels)
         x_ref, y_ref = x_vals[idx_rightmost_point], y_vals[idx_rightmost_point]
         label = ax_measured.annotate(label_text, (x_ref, y_ref),
                                       textcoords="offset points", xytext=(2,0),
@@ -160,7 +162,7 @@ label_ref2 = ax_ref2.annotate('PdCl2\nreference', xy=(1, 0.5),
 
 ax_ref3.tick_params(axis='y', which='both', left=False, labelleft=False)
 #plt.setp(ax_ref3.get_xticklabels(), visible=False)
-ax_ref3.set(xlim=[2, 41], xlabel="2theta, degree")
+ax_ref3.set(xlabel="2theta, degree")
 ax_ref3.set(ylim=[0, 110])
 data = np.loadtxt(os.path.join(reference_peaks_dir, references_fnames['CuCl']))
 twotheta, intensity = data[:,0], data[:,1]