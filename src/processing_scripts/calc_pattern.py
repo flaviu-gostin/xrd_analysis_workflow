f654027b1506879285c4d19d5757110d66226063
Flaviu Gostin <pfg@posteo.org>
2020-09-06 13:56:43 +0100

    Calculate peaks with different lattice constant

--- src/processing_scripts/calc_pattern.py
+++ src/processing_scripts/calc_pattern.py
@@ -1,6 +1,11 @@
 """ Calculate X-ray diffraction peaks from a given structure
 
-Usage: python calculate_pattern.py cif_file result_file
+Usage: python calculate_pattern.py cif_file result_file [lattice_ct]
+
+cif_file : CIF file for structure for which to calculate diffraction peaks
+result_file : save peaks to this file
+lattice_ct : change the lattice constant to this value in Angstrom (only makes
+             sense for cubic systems)
 
 """
 
@@ -19,6 +24,13 @@ two_theta_range = (0, 180)
 cif_fname = sys.argv[1]
 result_fname = sys.argv[2]
 structure = Structure.from_file(cif_fname)
+
+try:
+    lattice_param = float(sys.argv[3])
+    structure.scale_lattice(lattice_param ** 3)
+except IndexError:
+    pass
+
 twotheta, intensity = calculate_pattern(structure, wavelength_Angstrom,
                                         two_theta_range)
 stacked_array = np.column_stack((twotheta,intensity))
