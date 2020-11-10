a332896b1c5fd3255459d327c0eed952014e5c9a
Flaviu Gostin <pfg@posteo.org>
2020-11-10 16:02:09 +0000

    Change name of file where hashes are stored

--- data/split_for_upload.sh
+++ data/split_for_upload.sh
@@ -6,9 +6,9 @@
 # split the file into 200M pieces for upload (errors for larger files on Zenodo)
 split -b 200M ${1} ${1}_
 
-# calculate the md5 for the file and its pieces for later checking
+# calculate the md5 for the file and its splits for later checking
 # also for the .nxs file
 for FILE in ${1}*
 do
-    md5sum $FILE >> ${1}.md5
+    md5sum $FILE >> hashes.md5
 done
