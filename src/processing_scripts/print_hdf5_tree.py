1b8ad53da870478b9b9dd14b5dc9e65ff6765d75
Flaviu Gostin <pfg@posteo.org>
2021-05-16 09:10:27 +0200

    Delete obvious comments

--- src/processing_scripts/print_hdf5_tree.py
+++ src/processing_scripts/print_hdf5_tree.py
@@ -10,14 +10,14 @@ FILENAME can be abcd.hdf or abcd.nxs
 import h5py
 import sys
 
-# Open the hdf5 file
+
 file = h5py.File(sys.argv[1], 'r')
 
 def find_all(name, object):
     """ What to print for each member of the tree """
 
-    if isinstance(object, h5py.Dataset):    # if the member is a Dataset ...
-        if object.shape == (1,):    # has one dimension with one single value
+    if isinstance(object, h5py.Dataset):
+        if object.shape == (1,):
             print('Dataset named: ', name, '(value =', object[()], ')')
         else:
             print('Dataset named: ', name, '(shape:', object.shape,
