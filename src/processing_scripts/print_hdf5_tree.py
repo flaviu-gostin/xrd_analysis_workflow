a3e7d5dac0e705c30293e9010468830154f9a3c9
Flaviu Gostin <pfg@posteo.org>
2021-05-16 09:08:23 +0200

    DOC: can be run on hdf and nxs files

--- src/processing_scripts/print_hdf5_tree.py
+++ src/processing_scripts/print_hdf5_tree.py
@@ -1,18 +1,17 @@
 """
-Print the entire hierarchy of a hdf5 file
+Print the entire hierarchy of a hdf5 file.
 
 Run with: python print_hdf5_tree.py FILENAME
 
-"""
+FILENAME can be abcd.hdf or abcd.nxs
 
+"""
 
 import h5py
 import sys
 
-
 # Open the hdf5 file
 file = h5py.File(sys.argv[1], 'r')
-
 
 def find_all(name, object):
     """ What to print for each member of the tree """
