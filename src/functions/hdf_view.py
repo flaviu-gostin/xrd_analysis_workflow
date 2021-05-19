a7b27644ca68fa75ee89543df04fe644c5edc7ed
Flaviu Gostin <pfg@posteo.org>
2021-05-19 14:30:57 +0200

    Move hdf5 functions together in one script

--- src/functions/hdf_view.py
+++ src/functions/hdf_view.py
@@ -1,12 +1,12 @@
-""" This script contains a function to extract one individual diffraction image
-from the one dataset inside hdf files as a numpy array.
+"""Get all sorts of data from hdf5 files.
 
-Use: from hdf_view import get_slice
+Get tree info (the structure of the hdf5 file).
+Get actual data sets contained inside the file.
+
+Use: from hdf_view import the_function_you_need
 
 """
-
 import h5py
-
 
 def get_slice(filename, dataset_path, n):
     """Extract one single slice from a .hdf file.  Counting starts at 0."""
@@ -15,3 +15,32 @@ def get_slice(filename, dataset_path, n):
     slice = dataset[n]
     file.close()
     return slice
+
+
+def print_hdf5_tree(fname):
+    """Print the entire hierarchy of a hdf5 file.
+
+    Note (May 2016):
+    Data from the diffraction detector at beamline I18 (Diamond, UK)
+    comes in files with extension 'hdf', but most metadata is in
+    complementary files with extension 'nxs'.  To get that metadata, run
+    this function on the 'nxs' file.
+
+    """
+    def find_all(name, object):
+        """ What to print for each member of the tree """
+
+        if isinstance(object, h5py.Dataset):
+            if object.shape == (1,):
+                print('Dataset named: ', name, '(value =', object[()], ')')
+            else:
+                print('Dataset named: ', name, '(shape:', object.shape,
+                      ', type:', object.dtype, ')')
+        elif isinstance(object, h5py.Group):
+            print('+ Group named: ', name)
+            for item in object.attrs.items():
+                print('Attribute: ', item)
+
+    file = h5py.File(fname, 'r')
+    file.visititems(find_all)
+    file.close()
